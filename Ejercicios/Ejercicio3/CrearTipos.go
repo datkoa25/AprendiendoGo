4bce3f4b6d8fe3907785d3e90934ab6f168097b4
Joshua Benavides <elkoalamc@gmail.com>
2024-04-08 10:30:30 -0500

    Conversiones

--- Ejercicios/Ejercicio3/CrearTipos.go
+++ Ejercicios/Ejercicio3/CrearTipos.go
@@ -19,4 +19,18 @@ func main() {
 	b = 1000
 	fmt.Println(b)
 	fmt.Printf("%T\n", b)
+
+	/*error - Recuerda que Go es un lenguaje estatico
+
+	a = b
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	*/
+
+	//Como usar la conversión con sintaxis T(x) donde T es un tipo y X es una expresión que puede ser convertida en el tipo T
+
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
 }
