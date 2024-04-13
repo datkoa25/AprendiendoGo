f93189b9816c87eb287d733d5d21a5d33b6a77e6
Joshua Benavides <elkoalamc@gmail.com>
2024-04-13 11:38:17 -0500

    ejercicio 4

--- Ejercicios/Ejercicio4/Ejercicio4.go
+++ Ejercicios/Ejercicio4/Ejercicio4.go
@@ -10,9 +10,9 @@ var y int = 8
 func main() {
 	if x > y {
 		fmt.Println(x, "es mayor que ", y)
-		fmt.Println(x >= y)
+		fmt.Println(x >= y) //Operador de comparación - Logica booleana
 	} else {
 		fmt.Println(x, "es menor que ", y)
-		fmt.Println(x <= y)
+		fmt.Println(x <= y) //Operador de comparación - Logica booleana
 	}
 }
