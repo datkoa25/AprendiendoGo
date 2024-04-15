6cdc2a5350a6437c67b53ec19e11d4199bcb7cd2
Joshua Benavides <elkoalamc@gmail.com>
2024-04-15 12:51:42 -0500

    ejercicio 6

--- Ejercicios/Ejercicio 6/Ejercicio6.go
+++ Ejercicios/Ejercicio 6/Ejercicio6.go
@@ -20,4 +20,25 @@ func main() {
 	fmt.Println(bs)
 	fmt.Printf("%T", bs)
 
+	//Unicode code point ej: U+20AC
+
+	fmt.Println("")
+
+	//for funciona para crear un bucle y i es igual a iterar o en otras palabras repetir
+	//for i, para inicializar, i marcar el condicional y i++ para declarar el post, i++ = i = i+1
+
+	for i := 0; i < len(s1); i++ {
+
+		fmt.Printf("%#U ", s1[i])
+	}
+
+	fmt.Println("")
+
+	for i, v := range s1 {
+
+		fmt.Printf("En el indice %d el valor es %v\n", i, v)
+	}
+
+	fmt.Printf("Con el verbo %q inidico que se imprima el string %s", "%s", s1)
+
 }
